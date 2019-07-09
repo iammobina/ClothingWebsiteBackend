f6973201ba8a48a42abddcc0b2f88353221f5fbf
Ghazaal <ghazal_edrisabadi@yahoo.com>
2019-07-09 15:28:30 +0430

    Add 'User' model(users/models) and change admin.py(users/admin).

--- Backend/users/models.py
+++ Backend/users/models.py
@@ -10,3 +10,18 @@ class Design(models.Model):
 
     def __str__(self):
         return self.Title
+
+class User(models.Model):
+    #User = models.OneToOneField(User,on_delete=models.CASCADE)
+    Photo = models.ImageField(upload_to='userimage/', blank=True)
+    Name = models.CharField(max_length=100, blank=False)
+    Lastname = models.CharField(max_length=100,blank=False)
+    userId = models.CharField(max_length=100,blank=False)
+    Password = models.CharField(max_length=100, blank=False)
+    Confirm = models.CharField(max_length=100, blank=False, help_text= 'Confirm your password')
+    Phone = models.CharField(max_length=15, blank=True)
+    Email = models.EmailField(max_length=100, blank=True)
+    Address = models.TextField(max_length=300, blank=True)
+
+    def __str__(self):
+        return self.userId
