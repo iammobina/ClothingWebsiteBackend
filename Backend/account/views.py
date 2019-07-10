e6fc835700aa5e41890fea8da7f796e86e026550
Mobina Kashanian <Mobina.km@gmail.com>
2019-07-10 09:37:03 +0430

    Add FileView function to view Files.

--- Backend/account/views.py
+++ Backend/account/views.py
@@ -43,3 +43,17 @@ def login_view(request):
             return redirect(next)
         return redirect("/")
     return render(request, "form.html", {"form": form, "title": title})
+
+
+class FileUploadView(APIView):
+    parser_class = (FileUploadParser,)
+
+    def post(self, request, *args, **kwargs):
+
+        file_serializer = FileSerializer(data=request.data)
+
+        if file_serializer.is_valid():
+            file_serializer.save()
+            return Response(file_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
