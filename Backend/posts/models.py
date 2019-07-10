f9648f6533395c28414e1fb17bedaa3a90751bf8
Mobina Kashanian <Mobina.km@gmail.com>
2019-07-10 09:09:48 +0430

    Add upload_location function to save images on a location.

--- Backend/posts/models.py
+++ Backend/posts/models.py
@@ -5,3 +5,9 @@ from django.utils import timezone
 class PostManager(models.Manager):
     def active(self, *args, **kwargs):
         return super(PostManager, self).filter(draft=False).filter(publish__lte=timezone.now())
+
+
+def upload_location(instance, filename):
+    PostModel = instance.__class__
+    new_id = PostModel.objects.order_by("id").last().id + 1
+    return "%s/%s" % (new_id, filename)
