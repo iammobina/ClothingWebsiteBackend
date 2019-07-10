7d92cf40dd8cbe31ed49dbecee30f051386501b5
Mobina Kashanian <Mobina.km@gmail.com>
2019-07-10 09:12:33 +0430

    Add pre_save function to have access of draft and pre_save.

--- Backend/posts/models.py
+++ Backend/posts/models.py
@@ -79,3 +79,16 @@ def create_slug(instance, new_slug=None):
         new_slug = "%s-%s" % (slug, qs.first().id)
         return create_slug(instance, new_slug=new_slug)
     return slug
+
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_slug(instance)
+
+    if instance.content:
+        html_string = instance.get_markdown()
+        read_time_var = get_read_time(html_string)
+        instance.read_time = read_time_var
+
+
+pre_save.connect(pre_save_post_receiver, sender=Post)
