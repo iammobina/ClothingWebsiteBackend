0107a15948329cf22babf7a3b0ac02a81075a0dd
Mobina Kashanian <Mobina.km@gmail.com>
2019-07-10 08:59:51 +0430

    Add url of post detail.

--- Backend/posts/urls.py
+++ Backend/posts/urls.py
@@ -4,10 +4,11 @@ from django.contrib import admin
 from .views import (
     post_list,
     post_create,
-
+    post_detail,
 )
 
 urlpatterns = [
     url(r'^$', post_list, name='list'),
     url(r'^create/$', post_create),
+    url(r'^(?P<slug>[\w-]+)/$', post_detail, name='detail'),
 ]
