eeec1a03bd1199cca0b51ef9ebf6fb5ee3d40d6d
Mobina Kashanian <Mobina.km@gmail.com>
2019-07-10 09:21:57 +0430

    Add UpdatePostApi class to serialize updated post.

--- Backend/posts/api/views.py
+++ Backend/posts/api/views.py
@@ -1,7 +1,8 @@
 from rest_framework.generics import (
-    CreateAPIView, RetrieveAPIView)
+    CreateAPIView, RetrieveAPIView, RetrieveUpdateAPIView)
 from rest_framework.permissions import AllowAny
 
+from posts.api.permissions import IsOwnerOrReadOnly
 from posts.models import Post
 
 from .serializers import (
@@ -22,3 +23,13 @@ class PostDetailAPIView(RetrieveAPIView):
     serializer_class = PostDetailSerializer
     lookup_field = 'slug'
     permission_classes = [AllowAny]
+
+
+class PostUpdateAPIView(RetrieveUpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostCreateUpdateSerializer
+    lookup_field = 'slug'
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def perform_update(self, serializer):
+        serializer.save(user=self.request.user)
