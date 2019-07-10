df2b275eeb33cb1b063e2e900de849f6263e93a8
Mobina Kashanian <Mobina.km@gmail.com>
2019-07-10 09:04:00 +0430

    Add url of post update.

--- Backend/posts/urls.py
+++ Backend/posts/urls.py
@@ -5,10 +5,12 @@ from .views import (
     post_list,
     post_create,
     post_detail,
-)
+    post_update)
 
 urlpatterns = [
     url(r'^$', post_list, name='list'),
     url(r'^create/$', post_create),
     url(r'^(?P<slug>[\w-]+)/$', post_detail, name='detail'),
+    url(r'^(?P<slug>[\w-]+)/edit/$', post_update, name='update'),
+    
 ]
