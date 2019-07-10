d12df675dd510c0dc0ff33c663ba7926c4dacd8e
mohammadreza <mrelmimiyab@gmail.com>
2019-07-09 19:28:35 -0700

    add request list class with query that recive all objects

--- Backend/Request/views.py
+++ Backend/Request/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from rest_framework import generics
+from Request.models import  Tailor,User,Request
+from Request.serializers import TailorSerializer,UserSerializer,RequestSerializer
 # Create your views here.
+
+
+class RequestList(generics.ListCreateAPIView):
+
+    queryset = Request.objects.all()
+    serializer_class = RequestSerializer
+
+
+class RequestDetail(generics.RetrieveUpdateDestroyAPIView):
+        queryset = Request.objects.all()
+        serializer_class = RequestSerializer
+
