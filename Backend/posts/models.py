e8f921b70df31bee6e0083560da86ae3eba196cd
Mobina Kashanian <Mobina.km@gmail.com>
2019-07-10 09:10:28 +0430

    Add Post Class to create PostModels in database

--- Backend/posts/models.py
+++ Backend/posts/models.py
@@ -11,3 +11,49 @@ def upload_location(instance, filename):
     PostModel = instance.__class__
     new_id = PostModel.objects.order_by("id").last().id + 1
     return "%s/%s" % (new_id, filename)
+
+
+class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to=upload_location,
+                              null=True,
+                              blank=False,
+                              width_field="width_field",
+                              height_field="height_field")
+    height_field = models.IntegerField(default=0)
+    width_field = models.IntegerField(default=0)
+    content = models.TextField()
+    draft = models.BooleanField(default=False)
+    publish = models.DateField(auto_now=False, auto_now_add=False)
+    read_time = models.IntegerField(default=0)  # models.TimeField(null=True, blank=True) #assume minutes
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    objects = PostManager()
+
+    def __unicode__(self):
+        return self.title
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("posts:detail", kwargs={"slug": self.slug})
+
+    def get_api_url(self):
+        return reverse("posts-api:detail", kwargs={"slug": self.slug})
+
+    class Meta:
+        ordering = ["-timestamp", "-updated"]
+
+    def get_markdown(self):
+        content = self.content
+        markdown_text = markdown(content)
+        return mark_safe(markdown_text)
+
+    def get_content_type(self):
+        instance = self
+        content_type = ContentType.objects.get_for_model(instance.__class__)
+        return content_type
