03ff4161c4b3287c0ed2958c6ec15d8576227063
Mobina Kashanian <Mobina.km@gmail.com>
2019-07-10 08:50:12 +0430

    Add "Post_detail" function to have (timezone) of created post.

--- Backend/posts/views.py
+++ Backend/posts/views.py
@@ -1,8 +1,13 @@
+from time import timezone
+
 import messages as messages
 
 from posts.forms import PostForm
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
+
+from posts.models import Post
+
 
 def post_create(request):
 	if not request.user.is_staff or not request.user.is_superuser:
@@ -20,3 +25,10 @@ def post_create(request):
 	}
 	return render(request, "post_form.html", context)
 
+
+def post_detail(request, slug=None):
+	instance = get_object_or_404(Post, slug=slug)
+	if instance.publish > timezone.now().date() or instance.draft:
+		if not request.user.is_staff or not request.user.is_superuser:
+			raise Http404
+	return render(request, "post_detail.html")
