28af1bd8caa2dcd771e31e5ea21ff3774122da05
Mobina Kashanian <Mobina.km@gmail.com>
2019-07-10 08:58:48 +0430

    Add url of post create.

--- Backend/posts/urls.py
+++ Backend/posts/urls.py
@@ -4,11 +4,10 @@ from django.contrib import admin
 from .views import (
     post_list,
     post_create,
-    post_detail,
-    post_update,
-    post_delete,
+
 )
 
 urlpatterns = [
     url(r'^$', post_list, name='list'),
+    url(r'^create/$', post_create),
 ]
