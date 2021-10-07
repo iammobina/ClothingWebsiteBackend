4f2669f98101dd22641534954fd5622928cb8301
Mobina Kashaniyan <51053506+iammobina@users.noreply.github.com>
2021-10-07 15:48:32 +0330

    Delete Backend directory

--- Backend/Request/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path,include
-from Request import views
-
-urlpatterns = [
-    path('requests/', views.RequestList.as_view()),
-    path('requests/<int:pk>/', views.RequestDetail.as_view()),
-]
-
